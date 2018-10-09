65cfc0814ee5c6a09160fdb8d13c55e7e8b38460
Alaa.Zain <Alaa.Zain@AZAIN-10-9694>
2018-10-09 12:34:14 +0200

    Add project files.

---
+++ MySql8_NetConnector8/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySql8_NetConnector8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (worldEntities ent = new worldEntities())
+            {
+                List<city>_l= ent.cities.ToList();              
+                dataGridView1.DataSource = _l;               
+            }
+                
+        }
+    }
+}
